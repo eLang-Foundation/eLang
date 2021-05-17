7325e19877aa8c8628a6766612cd91fb3955c4da
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-05-18 00:53:36 +0300

    removed junk

--- src/get.cpp
+++ src/get.cpp
@@ -3,21 +3,12 @@
 #include <cstring>
 #include <bits/stdc++.h>
 
-bool replace(std::string& str, const std::string& from, const std::string& to) {
-	size_t start_pos = str.find(from);
-	if(start_pos == std::string::npos)
-		return false;
-	str.replace(start_pos, from.length(), to);
-	return true;
-}
-
 // this function returns the string found using given regex pattern
 char *get(char *string, char *pattern)
 {
 	std::string s (string);
 	std::regex e (pattern);
 	std::smatch m;
-	std::string returnString = s;
 
 	if (std::regex_search(s, m, e))
 	{
