523f564ded641c8dde2747b7eb276c32867792be
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-05-25 03:51:19 +0300

    added tests for re.cpp

---
+++ tests/re_test.c
@@ -0,0 +1,19 @@
+#include "../src/re.h"
+#include "Unity/src/unity.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void re_test(void)
+{
+	TEST_ASSERT(match("hello", "[\\w]*") == true);
+	char *string = "hello";
+	TEST_ASSERT(get("123hello456", "([\\w]+)") == string);
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(re_test);
+	return UNITY_END();
+}
