584b37c7bb1f000284b481c842b39f4eafa6756e
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-05-25 03:21:25 +0300

    added tests for insideQuotes.c

---
+++ tests/insideQuotes_test.c
@@ -0,0 +1,19 @@
+#include "insideQuotes.h"
+#include "Unity/src/unity.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void insideQuotes_test(void)
+{
+	TEST_ASSERT(insideQuotes(1, "\"Test") == true);
+	TEST_ASSERT(insideQuotes(2, "hello world") == false);
+	TEST_ASSERT(insideQuotes(3, "he'123") == true);
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(insideQuotes_test);
+	return UNITY_END();
+}
