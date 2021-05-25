b9f914783718bc7f88328fab6e708ffbe6b56484
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-05-25 04:10:47 +0300

    fixed a bug

--- tests/re_test.c
+++ tests/re_test.c
@@ -1,14 +1,19 @@
 #include "../src/re.h"
 #include "Unity/src/unity.h"
+#include <string.h>
 
 void setUp(void) {}
 void tearDown(void) {}
 
 void re_test(void)
 {
-	TEST_ASSERT(match("hello", "[\\w]*") == true);
-	char *string = "hello";
-	TEST_ASSERT(get("123hello456", "([\\w]+)") == string);
+	char *string1 = "hello";
+	char *pattern1 = "[\\w]*";
+	TEST_ASSERT(match(string1, (char *)pattern1) == true);
+	char *string2 = "123hello456";
+	char *pattern2 = "([A-Za-z]+)";
+	char *result = "hello";
+	TEST_ASSERT_EQUAL_STRING(result, get(string2, pattern2));
 }
 
 int main(void)
