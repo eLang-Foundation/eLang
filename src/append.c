bd02edc2ac6f901b198926766ad8bc71796d48e0
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-01 22:20:24 +0300

    added appendString

--- src/append.c
+++ src/append.c
@@ -10,3 +10,20 @@ char *appendChar(char *string, char chr)
 	strcpy(string, tmp);
 	return string;
 }
+
+// this function appends the given string to the given string
+char *appendString(char *string, char *string2)
+{
+	size_t length = strlen(string);
+	size_t length2 = strlen(string2);
+	char tmp[length + length2 + 1];
+	strcpy(tmp, string);
+	for (int i = 0; i < length2; i++)
+	{
+		tmp[length++] = string2[i];
+	}
+	tmp[length++] = '\0';
+	string = realloc(string, length);
+	strcpy(string, tmp);
+	return string;
+}
