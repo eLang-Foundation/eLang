15e9a51af2ac8c25a19e714e08a1b2b34a221e16
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-03 13:03:37 +0300

    adding a new line before the error message

--- src/helpers/raiseError.c
+++ src/helpers/raiseError.c
@@ -1,6 +1,7 @@
 // function for raising errors
 void raiseError(char *errorType, char *error, char *line, int lineNumber, char *filename)
 {
+	printf("\n");
 	// if the incorrect line of code was given
 	if (line != NULL)
 	{
