b37d75998d336f6c66dcf4f7bf00d67dedefae2f
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-08 22:41:01 +0300

    Added toString.c
    This file converts the given value to string

---
+++ src/stdlib/toString.c
@@ -0,0 +1,22 @@
+// this function converts the given variable to string
+char *toString(char *var, int numberOfVariables)
+{
+	for (int i = 0; i < numberOfVariables; i++)
+	{
+		Variable currentVariable = VARIABLES[i];
+		if (!strcmp(currentVariable.name, var))
+		{
+			if (!strcmp(type(currentVariable.value), "string"))
+			{
+				char *string = malloc(1);
+				strcpy(string, "");
+				for (int h = 1, l = strlen(currentVariable.value); h < l - 1; h++)
+				{
+					string = appendChar(string, currentVariable.value[h]);
+				}
+				return string;
+			}
+		}
+	}
+	return strdup("");
+}
