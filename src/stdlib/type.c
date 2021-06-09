065c74d561bb95e975d219520878da168b54a2fe
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-09 13:57:25 +0300

    converted indentations to tabs

--- src/stdlib/type.c
+++ src/stdlib/type.c
@@ -1,12 +1,12 @@
 // this function returns the type of the given value
 char *type(char *value)
 {
-    int index = strlen(value) - 1;
-    if ((value[0] == '"' && value[index] == '"') ||
-        (value[0] == '\'' && value[index] == '\'') ||
-        (value[0] == '`' && value[index] == '`'))
-    {
-        return "string";
-    }
-    return "undefined";
+	int index = strlen(value) - 1;
+	if ((value[0] == '"' && value[index] == '"') ||
+		(value[0] == '\'' && value[index] == '\'') ||
+		(value[0] == '`' && value[index] == '`'))
+	{
+		return "string";
+	}
+	return "undefined";
 }
