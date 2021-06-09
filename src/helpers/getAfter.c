9d746d5aa210f2b7079b71ef6876ed21aa28119c
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-09 16:03:39 +0300

    removed the numberOfLines argument
    it was stupid to include it because I can get the number of lines just by getting the lines.length value because lines is of type strArray (but maybe I just neeed it before and forgot to remove it)

--- src/helpers/getAfter.c
+++ src/helpers/getAfter.c
@@ -1,6 +1,7 @@
 // this function extracts code from given string after (and including) line at the given index
-char *getAfter(strArray lines, int index, int numberOfLines)
+char *getAfter(strArray lines, int index)
 {
+	int numberOfLines = lines.length;
 	char *after = malloc(1);
 	strcpy(after, "");
 	for (ui j = index; j < numberOfLines; j++)
