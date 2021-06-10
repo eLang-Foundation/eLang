8e79cf7425b8a62cb3127f51944df58ac81a67ce
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-10 21:48:37 +0300

    added calculate.c

---
+++ src/stdlib/calculate.c
@@ -0,0 +1,35 @@
+// this function calculates the given expression
+double calculate(char *expression)
+{
+	char *tmp1 = get(expression, "([\\w\\W]+?)[\\+\\-\\*\\/]+");
+	char *operator = get(expression, "[\\w\\W]+?([\\+\\-\\*\\/]+)");
+	char *tmp2 = get(expression, "[\\+\\-\\*\\/]+([\\w\\W]+)");
+
+	double number1 = toNumber(getValue(trim(tmp1)));
+	double number2 = toNumber(getValue(trim(tmp2)));
+
+	free(tmp1);
+	free(tmp2);
+
+	if (!strcmp(operator, "+"))
+	{
+		return number1 + number2;
+	}
+
+	if (!strcmp(operator, "-"))
+	{
+		return number1 - number2;
+	}
+
+	if (!strcmp(operator, "*"))
+	{
+		return number1 * number2;
+	}
+
+	if (!strcmp(operator, "/"))
+	{
+		return number1 / number2;
+	}
+
+	return 0;
+}
