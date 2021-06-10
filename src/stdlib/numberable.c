4e733bbd6981f105ae066f75d28cbc287a5ef649
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-10 22:27:03 +0300

    made the thing a whole lot easier

--- src/stdlib/numberable.c
+++ src/stdlib/numberable.c
@@ -1,22 +1,5 @@
 // this function checks if the given value can be converted to a number
 bool numberable(char *value)
 {
-	size_t length = strlen(value);
-
-	bool numberable = false;
-
-	for (int i = 0; i < length; i++)
-	{
-		if (((int) value[i] >= 48 && (int) value[i] <= 57) || (int) value[i] == 46)
-		{
-			numberable = true;
-		}
-		else
-		{
-			numberable = false;
-			break;
-		}
-	}
-	
-	return numberable;
+	return match(value, "[\\d\\.]+");
 }
