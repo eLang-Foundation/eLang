191f2f719b74982ab00c4f39f4a4f203ebb6d7c7
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-11 16:12:12 +0300

    using the strtod function

--- src/stdlib/toNumber.c
+++ src/stdlib/toNumber.c
@@ -3,54 +3,11 @@ double toNumber(char *string)
 {
     if (numberable(string))
 	{
-		double returnValue = 0;
-
-		size_t length = strlen(string);
-
-		bool floatValue = false;
-
-		if (count('.', string)) floatValue = true;
-
-		if (floatValue)
-		{
-			char *parts[2];
-
-			int index = 0;
-
-			char *part = strtok(string, ".");
-			while (part != NULL)
-			{
-				parts[index++] = strdup(part);
-				part = strtok(NULL, ".");
-			}
-
-			for (int i = (int) strlen(parts[0]) - 1; i >= 0; i--)
-			{
-				returnValue += ((int) parts[0][i] - 48) * pow(10, i);
-			}
-			free(parts[0]);
-
-			if (index > 1)
-			{
-				for (int i = 0, l = (int) strlen(parts[1]); i < l; i++)
-				{
-					returnValue += ((int) parts[1][i] - 48) / pow(10, i + 1);
-				}
-				free(parts[1]);
-			}
-		}
-
-		else
-		{
-			for (int i = 0; i < length; i++)
-			{
-				int n = (int) string[i] - 48;
-				if (n == 0) returnValue *= 10;
-				else returnValue += n * (i > 1 ? pow(10, i) : 1);
-			}
-		}
-
-		return returnValue;
+		puts(string);
+		double number;
+		char *ptr;
+		number = strtod(string, &ptr);
+		return number;
 	}
 
 	char error[] = "Couldn't convert \"";
