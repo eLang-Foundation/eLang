134dcf7d33073a6cb08b84b7b067b3241ef9a0f6
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-14 17:15:12 +0300

    redesigned the algorithm

--- src/stdlib/calculate.c
+++ src/stdlib/calculate.c
@@ -1,3 +1,14 @@
+void addNumber(double *numbers, char *currentNumberString, int *numberOfNumbers)
+{
+	currentNumberString = evaluate(trim(currentNumberString));
+	currentNumberString = getValue(currentNumberString);
+
+	double number = toNumber(currentNumberString);
+
+	numbers = realloc(numbers, ++*numberOfNumbers * sizeof(double));
+	numbers[*numberOfNumbers - 1] = number;
+}
+
 // this function calculates the given expression
 double calculate(char *expression)
 {
@@ -11,46 +22,55 @@ double calculate(char *expression)
 
 	bool skipNext = false;
 
-	for (int i = 0; i < length; i++)
+	char *currentNumberString = strdup("");
+
+	int wait = 0;
+
+	for (ui i = 0; i < length; i++)
 	{
-		switch (expression[i])
+		char currentChar = expression[i];
+
+		if (currentChar != '+' && currentChar != '-' && currentChar != '*' && currentChar != '/')
 		{
-			case '+':
-				operators[numberOfOperators++] = '+';
-				skipNext = false;
-				break;
-			case '-':
-				operators[numberOfOperators++] = '-';
-				skipNext = false;
-				break;
-			case '*':
-				operators[numberOfOperators++] = '*';
-				skipNext = false;
-				break;
-			case '/':
-				operators[numberOfOperators++] = '/';
-				skipNext = false;
-				break;
-			default:
-				if (!skipNext)
-				{
-					char number[100];
-					int j = 0;
-					for (; j < length; j++)
-					{
-						if (expression[i + j] == '+' || expression[i + j] == '-' || expression[i + j] == '*' || expression[i + j] == '/')
-							break;
-						number[j] = expression[i + j];
-					}
-					number[j] = '\0';
-					double num = toNumber(trim((char *) &number));
-					numbers = realloc(numbers, ++numberOfNumbers * sizeof(double));
-					numbers[numberOfNumbers - 1] = num;
-					skipNext = true;
-				}
-				break;
+			currentNumberString = appendChar(currentNumberString, currentChar);
+
+			if (currentChar == '(' && !insideQuotes(i, expression))
+			{
+				wait++;
+			}
+
+			if (currentChar == ')' && !insideQuotes(i, expression))
+			{
+				if (!--wait)
+					skipNext = false;
+			}
+		}
+
+		else
+		{
+			if (!skipNext)
+			{
+				operators[numberOfOperators++] = currentChar;
+
+				currentNumberString = strdup(trim(currentNumberString));
+
+				if (numberOfNumbers > 0) currentNumberString = appendChar(currentNumberString, ')');
+
+				addNumber(numbers, currentNumberString, &numberOfNumbers);
+				free(currentNumberString);
+				currentNumberString = strdup("");
+
+				skipNext = true;
+			}
+			else
+			{
+				currentNumberString = appendChar(currentNumberString, currentChar);
+			}
 		}
 	}
+
+	addNumber(numbers, currentNumberString, &numberOfNumbers);
+	free(currentNumberString);
 
 	double returnValue = 0;
 
