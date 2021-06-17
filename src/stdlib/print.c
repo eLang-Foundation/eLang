c394d0bc9e9466de2a91b6bfb9b3a8079621bd1d
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-17 15:52:04 +0300

    updated print.c

--- src/stdlib/print.c
+++ src/stdlib/print.c
@@ -6,7 +6,6 @@ void print(char **args, int numberOfArguments)
 	for (int k = 0; k < numberOfArguments; k++)
 	{
 		char *currentArgument = args[k];
-
 		currentArgument = getValue(currentArgument);
 
 		if (!strcmp(type(currentArgument), "String"))
@@ -14,30 +13,25 @@ void print(char **args, int numberOfArguments)
 			char *tmp = convertValueToString(currentArgument);
 			string = appendString(string, tmp);
 			free(tmp);
+			free(currentArgument);
 		}
 		else
 		{
-			char *tmp = evaluate(currentArgument);
-			if (strcmp(tmp, ""))
+			char *convertedToString = toString(currentArgument);
+			free(currentArgument);
+			if (strcmp(convertedToString, ""))
 			{
-				char *convertedToString = toString(getValue(tmp));
-				free(tmp);
-				if (strcmp(convertedToString, ""))
-				{
-					string = appendString(string, convertedToString);
-					free(convertedToString);
-				}
-				else
-				{
-					free(convertedToString);
-					char error[] = "Name \"";
-					strcat(error, currentArgument);
-					strcat(error, "\" is not defined");
-					raiseError(INS, error, LINES.array[lineNumber - 1], lineNumber, FILENAME);
-				}
+				string = appendString(string, convertedToString);
+				free(convertedToString);
 			}
 			else
-				free(tmp);
+			{
+				free(convertedToString);
+				char error[] = "Name \"";
+				strcat(error, currentArgument);
+				strcat(error, "\" is not defined");
+				raiseError(INS, error, LINES.array[lineNumber - 1], lineNumber, FILENAME);
+			}
 		}
 	}
 
