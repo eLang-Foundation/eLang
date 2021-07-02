28e8844a36f1603d4aa43ebb2a17cf691e6da27f
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-07-03 01:40:24 +0500

    updated getAfter.c

--- src/helpers/getAfter.c
+++ src/helpers/getAfter.c
@@ -1,18 +1,14 @@
 // this function extracts code from given string after (and including) line at the given index
 char *getAfter(strArray lines, int index)
 {
-	int numberOfLines = lines.length;
-	char *after = malloc(1);
-	strcpy(after, "");
+	ui numberOfLines = lines.length;
+	char *after = strdup("");
 	for (ui j = index; j < numberOfLines; j++)
 	{
-		if (strcmp(lines.array[j], "") != 0)
-		{
-			char *currentLine = strdup(lines.array[j]);
-			after = appendString(after, currentLine);
-			after = appendChar(after, '\n');
-			free(currentLine);
-		}
+		char *currentLine = strdup(lines.array[j]);
+		after = appendString(after, currentLine);
+		after = appendChar(after, '\n');
+		free(currentLine);
 	}
 	return after;
 }
