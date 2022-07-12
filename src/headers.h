1bc9c44746af268d7269a8c5cd88ecbc1c25fc89
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2022-07-12 22:36:42 +0500

    updated

--- src/headers.h
+++ src/headers.h
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <fstream>
 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
 using namespace std;
 
 
@@ -10,5 +15,10 @@ extern ifstream SRC_FILE;
 extern string SRC_CODE;
 
 
+extern vector<string> instructions;
+extern vector<string> names;
+
+
 #include "cleanup.cpp"
+#include "execute.cpp"
 
